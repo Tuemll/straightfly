a6203f1042d70b28e23d46ad4bf4de6a81adda49
Tueml <149439628+Tuemll@users.noreply.github.com>
2024-07-20 21:26:31 -0700

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -97,34 +97,34 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record on the most recent version of the game
+                        - Achieved the record on the most recent version of the game
                     </p>
                     <p>
-                        Achieved the record without using hacks (any FPS is fine, however physics bypass is banned)
+                        - Achieved the record without using hacks (any FPS up to 360 is fine, CBF is banned for now)
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        - Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count. 2 player levels require a handcam
+                        - Have either source audio or clicks/taps in the video. Edited audio only does not count. 2 player levels require a handcam
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        - The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt.
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated
+                        - The recording must also show the player hit the endwall, or the completion will be invalidated
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        - Do not use secret routes or bug routes
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        - Do not use easy modes, only a record of the unmodified level qualifies
                     </p>
                     <p>
-                        Once a level falls into Pinewood, we no longer accept records
+                        - Once a level falls into Pinewood, we no longer accept records
                     </p>
                     <p>
-                        2 player challenges cannot be completed with one hand, so no rebinding to adjacent keys
+                        - 2 player challenges cannot be completed with one hand, so no rebinding to adjacent keys
                     </p>
                      <p>
                     </p>
